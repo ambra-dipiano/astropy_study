3f62d353b626aebf14c72a1a55f2acfc83baf1e8
ambra-dipiano <ambra.dipiano@inaf.it>
2022-01-11 17:25:12 +0100

    fix patches with wcs

--- plot_countsmap.py
+++ plot_countsmap.py
@@ -18,7 +18,7 @@ trange = [0, 1200]
 erange = [0.03, 150]
 roi = 5
 pixelsize = 0.02
-target = {'ra': 83.6331, 'dec': 22.0145}
+target = {'ra': 83.6331, 'dec': 22.0145, 'rad': 0.2}
 pointing = {'ra': 83.6331, 'dec': 22.5145}
 
 # params plot
@@ -34,5 +34,7 @@ plot.counts_map(file=obs, trange=trange, erange=erange, pixelsize=pixelsize, roi
 # map with wcs celestial projection
 img = obs.replace('.fits', '_sky3.png')
 plot.plot_fits_skymap(file=sky, name=img, figsize=figsize, fontsize=fontsize)
-#plot.counts_map_with_wcs(file=obs, trange=trange, erange=erange, pixelsize=pixelsize, roi=roi, name=img, figsize=figsize, fontsize=fontsize)
+img = obs.replace('.fits', '_sky4_wreg.png')
+regions = [{'ra': 84.1205639560909, 'dec': 22.62576046697816, 'rad': 0.2}, {'ra': 83.85004186955878, 'dec': 22.964984433951212, 'rad': 0.2}, {'ra': 83.41615813044122, 'dec': 22.964984433951212, 'rad': 0.2}, {'ra': 83.1456360439091, 'dec': 22.625760466978157, 'rad': 0.2}]
+plot.plot_fits_skymap_with_regions(file=sky, regions=regions, name=img, figsize=figsize, fontsize=fontsize)
 
--- save_countsmap.py
+++ save_countsmap.py
@@ -8,6 +8,7 @@
 # *******************************************************************************
 
 from sagsci.tools.fits import Fits
+from sagsci.tools.plotting import SkyImage
 
 dl3 = './data/crab_offax.fits'
 template = './data/counts_map_template.fits'
@@ -16,3 +17,10 @@ pixelsize = 0.02
 
 m = Fits()
 dl4 = m.get_countmap_in_fits(dl3_file=dl3, template=template, maproi=maproi, pixelsize=pixelsize)
+
+plot = SkyImage()
+plot.set_target(ra=83.6331, dec=22.0145).set_pointing(ra=83.6331, dec=22.5145)
+regions = [{'ra': 84.1205639560909, 'dec': 22.62576046697816, 'rad': 0.2}, {'ra': 83.85004186955878, 'dec': 22.964984433951212, 'rad': 0.2}, {'ra': 83.41615813044122, 'dec': 22.964984433951212, 'rad': 0.2}, {'ra': 83.1456360439091, 'dec': 22.625760466978157, 'rad': 0.2}]
+plot.plot_fits_skymap_with_regions(file=dl4, regions=regions, name='./data/test.png', figsize=(5,5), fontsize=10)
+
+del m, plot
